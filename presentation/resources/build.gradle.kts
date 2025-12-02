e7b79eff644c0d9f1ee4f4a99fb76438d3fae562
jubin <108816562+GarethJB@users.noreply.github.com>
2025-12-02 15:27:54 +0900

    init: Presentation:resources

---
+++ presentation/resources/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("android-library-convention")
+    id("kotlin-options-convention")
+}
+
+android {
+    namespace = "com.jb.pixelquest.presentation.resources"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    
+}
+
