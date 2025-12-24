4d8b3af1fd12f6fe491df20781fc2efc54fad3c1
jubin <108816562+GarethJB@users.noreply.github.com>
2025-12-24 10:28:45 +0900

    Revert "feat: KMP 이미지 리소스 관리 전략 수립 및 구조 준비"
    This reverts commit 55e378bdf467bde6fa75a36b0361c9b345b3229e.

--- shared/presentation/resources/build.gradle.kts
+++ shared/presentation/resources/build.gradle.kts
@@ -1,26 +1,15 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
     id("com.android.library")
-    // android-library-convention은 kotlin-android를 적용하므로 KMP 모듈에서는 사용하지 않음
+    id("android-library-convention")
     id("kotlin-options-convention")
-    // Compose Multiplatform Resources는 플러그인 없이 라이브러리만 사용
 }
 
 android {
     namespace = "com.jb.pixelquest.shared.presentation.resources"
-    
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    
+
     defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
-    }
-    
-    compileOptions {
-        sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
-        targetCompatibility = org.gradle.api.JavaVersion.VERSION_11
     }
 
     buildTypes {
@@ -30,27 +19,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-        }
-    }
-}
-
-kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
-
-    sourceSets {
-        val commonMain by getting {
-            // Compose Multiplatform Resources는 향후 KMP 확장 시 사용 예정
-            // 현재는 Android 리소스 시스템 사용
-        }
-        
-        val androidMain by getting {
-            // Android 리소스는 src/main/res에서 계속 사용 가능
         }
     }
 }
